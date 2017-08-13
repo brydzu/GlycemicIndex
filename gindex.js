061bf7c0d54678ae0a7b0c1288564fb1600bd0c5
Dominik Lipinski <maboonga@gmail.com>
2017-08-13 07:29:50 +0200

    files

---
+++ gindex.js
@@ -0,0 +1,14 @@
+(function (){
+
+// var url = "http://localhost/foodTables.json";
+// var foodTables = new XMLHttpRequest();
+// foodTables.open("GET", url);
+// foodTables.send(null);
+
+// temporary solution for testing
+var foodTables = [{name: "Cheese", GI: 0}, {name: "Meats", GI: 0}, {name: "Broccoli", GI: 15},
+{name: "Coconut", GI: 35}]
+
+
+
+})()
