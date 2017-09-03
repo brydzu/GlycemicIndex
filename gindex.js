d29519a127fcda40ab5d5c48de6d955d72279291
Dominik Lipinski <maboonga@gmail.com>
2017-09-03 07:35:50 +0200

    very very primitive real time search but it works

--- gindex.js
+++ gindex.js
@@ -7,14 +7,15 @@
 
 // temporary solution for testing
 var foodTables = [{name: "Cheese", GI: 0}, {name: "Meats", GI: 0}, {name: "Broccoli", GI: 15},
-{name: "Coconut", GI: 35}];
+{name: "Coconut", GI: 35}, {name: "Bread Montignac", GI: 34}, {name:"Bread ezekiel", GI: 35}, 
+{name: "Bread white" , GI: 60}, {name: "Bread", GI: 55} ];
 
 function showFoods() {
     
     var newInput = document.getElementById("textInput").value.toLowerCase();
-
+    
     for(var i =0; i<foodTables.length; i++){
-        if(newInput == foodTables[i].name.toLowerCase()){
+        if(foodTables[i].name.toLowerCase().indexOf(newInput)!==-1){
         document.getElementById("results").innerHTML = foodTables[i].name +" "+ foodTables[i].GI;
         }
     }
