64b8e58e1796e96f3380572e715d2671cb593580
Dominik Lipinski <maboonga@gmail.com>
2017-09-04 06:42:13 +0200

    styling in JS works but all background has one color..

--- gindex.js
+++ gindex.js
@@ -1,21 +1,23 @@
 (function (){
 
-var url = "http://undefined2055.eu/GlycemicIndex/foodTables.json";
-var request = new XMLHttpRequest();
-request.open("GET", url);
-request.send(null);
+// var url = "http://undefined2055.eu/GlycemicIndex/foodTables.json";
+// var request = new XMLHttpRequest();
+// request.open("GET", url);
+// request.send(null);
 
 // why doesn't work when this variable is defined outside of showFoods? the same situation with other app...
 //var foodTables = JSON.parse(request.responseText);
 
 // temporary solution for testing
-// var foodTables = [{name: "Cheese", GI: 0}, {name: "Meats", GI: 0}, {name: "Broccoli", GI: 15},
-// {name: "Coconut", GI: 35}, {name: "Bread Montignac", GI: 34}, {name:"Bread ezekiel", GI: 35}, 
-// {name: "Bread white" , GI: 60}, {name: "Bread", GI: 55} ];
+var foodTables = [{name: "Cheese", GI: 0}, {name: "Meats", GI: 0}, {name: "Broccoli", GI: 15},
+{name: "Coconut", GI: 35}, {name: "Bread Montignac", GI: 34}, {name:"Bread ezekiel", GI: 35}, 
+{name: "Bread white" , GI: 60}, {name: "Bread", GI: 55} ];
+
+var foodBackgrundColor = document.querySelector("#results");
 
 function showFoods() {
     
-    var foodTables = JSON.parse(request.responseText);
+    //var foodTables = JSON.parse(request.responseText);
     var newInput = document.getElementById("textInput").value.toLowerCase();
     
     var matchingFoods = [];
@@ -23,9 +25,21 @@ function showFoods() {
     for(var i =0; i<foodTables.length; i++){
         if(foodTables[i].name.toLowerCase().indexOf(newInput)!==-1){  
             matchingFoods += foodTables[i].name +" "+ foodTables[i].GI +"</p>";
+            
+            if (foodTables[i].GI<35){
+                foodBackgrundColor.style.backgroundColor = "#009933";
+            } else if (foodTables[i].GI<55) {
+                foodBackgrundColor.style.backgroundColor = "#ff9933";
+            } else {
+                foodBackgrundColor.style.backgroundColor = "#cc0000";
             }
         }
+    }
+        
     document.getElementById("results").innerHTML = matchingFoods;
+    // if(foodTables[i].GI<35){
+    //     foodBackgrundColor.style.backgroundColor = "#ff0000";}
+
     }
 
 document.getElementById("textInput").oninput = showFoods;
