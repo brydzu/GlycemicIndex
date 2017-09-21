390770df2223703cacacd2aac36c2bfe41648f00
Dominik Lipinski <maboonga@gmail.com>
2017-09-21 15:18:23 +0200

    proper XMLHTTPRequest and properJSON

--- gindex.js
+++ gindex.js
@@ -1,21 +1,22 @@
 (function (){
 
-// var url = "http://undefined2055.eu/GlycemicIndex/foodTables.json";
-// var request = new XMLHttpRequest();
-// request.open("GET", url);
-// request.send(null);
+var url = "http://undefined2055.eu/GlycemicIndex/foodTables.json";
+var request = new XMLHttpRequest();
+request.onload = requestListener();
+request.open("GET", url);
+request.send(null);
 
+function requestListener() {
 // why doesn't work when this variable is defined outside of showFoods? the same situation with other app...
 //var foodTables = JSON.parse(request.responseText);
-
 // temporary solution for testing
-var foodTables = [{name: "Cheese", GI: 0}, {name: "Meats", GI: 0}, {name: "Broccoli", GI: 15},
-{name: "Coconut", GI: 35}, {name: "Bread Montignac", GI: 34}, {name:"Bread ezekiel", GI: 35}, 
-{name: "Bread white" , GI: 60}, {name: "Bread", GI: 55} ];
+// var foodTables = [{name: "Cheese", GI: 0}, {name: "Meats", GI: 0}, {name: "Broccoli", GI: 15},
+// {name: "Coconut", GI: 35}, {name: "Bread Montignac", GI: 34}, {name:"Bread ezekiel", GI: 35}, 
+// {name: "Bread white" , GI: 60}, {name: "Bread", GI: 55} ];
 
 function showFoods() {
     
-    //var foodTables = JSON.parse(request.responseText);
+    var foodTables = JSON.parse(request.responseText);
     var newInput = document.getElementById("textInput").value.toLowerCase(),
         matchingFoods = [],
         resultsList = document.getElementById("results");
@@ -48,14 +49,13 @@ function showFoods() {
                 }
                 return element.className;
             }
+            
     resultsList.appendChild(element);        
-    //element.innerHTML = "<div style="+changeColor()+">"+item.foodName+" "+item.gIndex+"</div>";
     element.innerHTML = "<div style="+changeColor()+">"+item.foodName+" "+
     '<span class = "displayedIndex">' +item.gIndex+ '</span>'+"</div>";        
         }
     })
-
 }
-
 document.getElementById("textInput").oninput = showFoods;
+}
 })()
