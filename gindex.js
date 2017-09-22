4d8b718791adfdb9fa1523b933bf1eb9eba2eac4
Dominik Lipinski <maboonga@gmail.com>
2017-09-22 13:26:05 +0200

    json typos corrected

--- gindex.js
+++ gindex.js
@@ -1,22 +1,23 @@
 (function (){
 
-var url = "http://undefined2055.eu/GlycemicIndex/foodTables.json";
-var request = new XMLHttpRequest();
-request.onload = requestListener();
-request.open("GET", url);
-request.send(null);
+// var url = "http://undefined2055.eu/GlycemicIndex/foodTables.json";
+// var request = new XMLHttpRequest();
+// request.onload = requestListener();
+// request.open("GET", url);
+// request.send(null);
 
-function requestListener() {
+// function requestListener() {
+
 // why doesn't work when this variable is defined outside of showFoods? the same situation with other app...
 //var foodTables = JSON.parse(request.responseText);
 // temporary solution for testing
-// var foodTables = [{name: "Cheese", GI: 0}, {name: "Meats", GI: 0}, {name: "Broccoli", GI: 15},
-// {name: "Coconut", GI: 35}, {name: "Bread Montignac", GI: 34}, {name:"Bread ezekiel", GI: 35}, 
-// {name: "Bread white" , GI: 60}, {name: "Bread", GI: 55} ];
+var foodTables = [{name: "Cheese", GI: 0}, {name: "Meats", GI: 0}, {name: "Broccoli", GI: 15},
+{name: "Coconut", GI: 35}, {name: "Bread Montignac", GI: 34}, {name:"Bread ezekiel", GI: 35}, 
+{name: "Bread white" , GI: 60}, {name: "Bread", GI: 55} ];
 
 function showFoods() {
     
-    var foodTables = JSON.parse(request.responseText);
+   // var foodTables = JSON.parse(request.responseText);
     var newInput = document.getElementById("textInput").value.toLowerCase(),
         matchingFoods = [],
         resultsList = document.getElementById("results");
@@ -57,5 +58,5 @@ function showFoods() {
     })
 }
 document.getElementById("textInput").oninput = showFoods;
-}
+//}
 })()
