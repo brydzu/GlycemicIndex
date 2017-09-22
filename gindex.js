ec538311a5366467ca706cf3b42bb887a6d41e30
Dominik Lipinski <maboonga@gmail.com>
2017-09-22 14:59:27 +0200

    styling

--- gindex.js
+++ gindex.js
@@ -13,7 +13,7 @@
 // temporary solution for testing
 var foodTables = [{name: "Cheese", GI: 0}, {name: "Meats", GI: 0}, {name: "Broccoli", GI: 15},
 {name: "Coconut", GI: 35}, {name: "Bread Montignac", GI: 34}, {name:"Bread ezekiel", GI: 35}, 
-{name: "Bread white" , GI: 60}, {name: "Bread", GI: 55} ];
+{name: "Bread white" , GI: 60}, {name: "Bread", GI: 55}];
 
 function showFoods() {
     
